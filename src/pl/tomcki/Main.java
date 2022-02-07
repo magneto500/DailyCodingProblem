bfc4ddadf5f04c80ec043b18dd9cac6276394c92
magneto500 <tomasz.piotr.grabowski@gmail.com>
2022-02-07 19:30:25 +0100

    daily coding problem 06.02

--- src/pl/tomcki/Main.java
+++ src/pl/tomcki/Main.java
@@ -1,21 +1,34 @@
 package pl.tomcki;
 
+/*
+06.02
+Given an array of integers in which two elements appear exactly once and all other elements appear exactly twice,
+find the two elements that appear only once.
+For example, given the array [2, 4, 6, 8, 10, 2, 6, 10], return 4 and 8.
+The order does not matter.
+*/
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Main {
 				public static void main(String[] args) {
-								int[] integerTable = {2, 4, 6, 8, 10, 2, 6, 10};
-								List<Integer> singleNumberIntegerList = new ArrayList<>();
 
+
+
+								Integer[] integerTable = {2, 4, 6, 8, 10, 2, 6, 10};
+								List<Integer> integerList = Arrays.asList(integerTable);
+								List<Integer> singleNumberIntegerList = new ArrayList<>();
 								for (int i = 0; i <8; i++) {
-												if(!singleNumberIntegerList.contains(integerTable[i])){
-																singleNumberIntegerList.add(integerTable[i]);
+												if(!singleNumberIntegerList.contains(integerList.get(i))){
+																singleNumberIntegerList.add(integerList.get(i));
 												} else
 												{
-																singleNumberIntegerList.remove(integerTable[i]);
+																singleNumberIntegerList.remove(integerList.get(i));
 												}
 								}
+								System.out.println(Arrays.toString(integerList.toArray()));
 								System.out.println(singleNumberIntegerList);
 				}
 }
